26e027595669ed0f4c8d5a48e2a98741cb4b9396
Grisha Ivanov <grisha.ivanov@rseg.com>
2020-11-06 09:09:18 -0700

    continued particle_physics.py docstrings, 3D viz, and added placeholder e-b field sign functions

--- pyparticles/modules/particle_env.py
+++ pyparticles/modules/particle_env.py
@@ -1,6 +1,13 @@
 import numpy as np
 
+def e_field_sign(particle_position: int):
+    return
+
+def b_field_sign(particle_position: int):
+    return
+
 def generate_environment(environment_size: np.array
                          ):
+    
 
     return generated_environment
